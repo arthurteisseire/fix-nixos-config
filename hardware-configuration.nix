5f831d56690b06b6eb62edc26da72c5ec5b54ae4
arthurteisseire <arthurteisseire33@gmail.com>
2024-11-05 08:30:27 +0100

    Enable hardware nvidia support

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -10,7 +10,7 @@
 
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sr_mod" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-amd" ];
+  boot.kernelModules = [ "kvm-amd" "nvidia" "nvidia_uvm" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
